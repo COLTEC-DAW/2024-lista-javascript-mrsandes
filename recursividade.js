1f594ab1c1469553681c893e019d9b344064b3eb
mrsandes <mtsandes@yahoo.com>
2024-06-19 10:49:09 -0300

    Recursividade

--- recursividade.js
+++ recursividade.js
@@ -2,9 +2,19 @@ function mod2(number) {
     return number % 2 == 0;
 }
 
-function mod(num, mod) {
-    return num % mod == 0;
+function mod(x, y) {
+    if (x == y) {
+        return true;
+    }
+
+    if (x <= y) {
+        return false;
+    }
+
+    else {
+        return mod(x - y, y);
+    }
 }
 
 console.log(mod2(10));
-console.log(mod(10, 5));
+console.log(mod(200, 13));
